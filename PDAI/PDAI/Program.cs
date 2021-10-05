ff9184ffd15da193a508cb18e61f5ccf0700389f
Theodor Risager <theodor349@gmail.com>
2021-10-05 11:21:52 +0200

    PDAI Project

---
+++ PDAI/PDAI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PDAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
