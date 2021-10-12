854beb86d8f5d6c7b3b892e73c60c30c1e42bbbf
Theodor Risager <theodor349@gmail.com>
2021-10-12 13:59:40 +0200

    Tests

---
+++ PDAI/Test/Unit/Parser.Pddl.Tests/TypesParserTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parser.Pddl.Tests
+{
+    [TestClass]
+    public class TypesParserTests
+    {
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        [TestMethod]
+        public void SingleType(int amount)
+        {
+
+        }
+    }
+}
