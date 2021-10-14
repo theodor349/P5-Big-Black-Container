8f2bddd7dd096302a0c89f897fdb6f6b0b864b3e
EmilGHenriksen <56338371+EmilGHenriksen@users.noreply.github.com>
2021-10-14 13:14:14 +0200

    Done with PredicateParser

--- PDAI/Shared/Models/Predicate.cs
+++ PDAI/Shared/Models/Predicate.cs
@@ -1,11 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Shared.Models
 {
     public class Predicate : Clause
     {
+        private Regex multipleTypeReg = new Regex(@"(\?\w*.)*-.\w*");
         public Predicate(string text, List<Entity> entities)
         {
+            text = text.Trim('(', ')');
+            Name = text.Split(" ").First();
+            text = text.Substring(Name.Length).Trim();
+            if (entities.Count > 1)
+            {
+                HandleMultipleTypes(text, entities);
+            }
+            else
+            {
+                HandleSingleType(text, entities);
+            }
+        }
+
+        private void HandleSingleType(string text, List<Entity> entities)
+        {
+            Parameters = GetParameters(text, entities);
+        }
+
+        private List<Parameter> GetParameters(string text, Entity entity)
+        {
+            var res = new List<Parameter>();
+            var words = text.Split("?");
+            foreach (var word in words)
+            {
+                if (string.IsNullOrWhiteSpace(word))
+                    continue;
+
+                res.Add(new Parameter()
+                {
+                    Name = word.Trim(),
+                    Entity = entity.FirstOrDefault(),
+                });
+            }
+            return res;
+        }
+
+        private void HandleMultipleTypes(string text, List<Entity> entities)
+        {
+            foreach (Match match in multipleTypeReg.Matches(text))
+            {
+                var split = match.Value.Split("-");
+                var t = entities.Where(x => x.Type.Equals(split[1].Trim())).FirstOrDefault();
+
+                var parameters = GetParameters(split[0], );
+
+            }
         }
     }
 }
