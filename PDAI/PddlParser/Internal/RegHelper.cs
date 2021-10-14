e48b084f3dabb14cf1a22360378388875e51a61e
Theodor Risager <theodor349@gmail.com>
2021-10-14 10:50:03 +0200

    Regex Helper

---
+++ PDAI/PddlParser/Internal/RegHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PddlParser.Internal
+{
+    internal static class RegHelper
+    {
+        private static List<string> GetActionIndecies(List<string> lines, Regex regex)
+        {
+            var line = string.Join(' ', lines).Replace("\t", "");
+            var res = regex.Matches(line).ToList().ConvertAll(x => TrimStateLine(x.Value));
+
+            return res;
+        }
+
+        private static string TrimStateLine(string line)
+        {
+            List<string> words = line.Split().ToList();
+            words.RemoveAt(0);
+            words.RemoveAt(words.Count - 1);
+            return string.Join(' ', words);
+        }
+    }
+}
