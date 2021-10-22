2ac51f727d8c791c8cf80739427938edeffd40a4
Alan Khorsid <alan18@hotmail.dk>
2021-10-22 10:05:32 +0200

    Just added tests for ex and bg

--- PDAI/Test/Unit/PopperWriter.Tests/BackgroundGeneratorTests.cs
+++ PDAI/Test/Unit/PopperWriter.Tests/BackgroundGeneratorTests.cs
@@ -24,35 +24,49 @@ namespace PopperWriter.Tests
             Assert.AreEqual(expected, result);
         }
 
+        [DynamicData("PredicateInput")]
         [DataTestMethod]
-        public void GetPredicates_validPredicate_CorrectString()
+        public void GetPredicates_validPredicate_CorrectString(List<string> names, List<List<PredicateOperator>> initialstates, List<List<PredicateOperator>> goalstates, List<string> expected)
         {
             BackgroundGenerator backgroundGenerator = new BackgroundGenerator();
-            List<string> names = new List<string>() { "p1", "p2", "p3" };
-            List<List<PredicateOperator>> initialstates = new List<List<PredicateOperator>>() {
-                Models.GetPredicateOperatorList(new List<string>() { "(on b1 b1)", "(calibration_target instrument1 star1)", "(power_avail satellite1)" }),
-                Models.GetPredicateOperatorList(new List<string>() { "(on b2 b2)", "(calibration_target instrument2 star2)", "(power_avail satellite2)" }),
-                Models.GetPredicateOperatorList(new List<string>() { "(on b3 b3)", "(calibration_target Instrument3 star3)", "(power_avail satellite3)" }),
-            };
-            List<List<PredicateOperator>> goalstates = new List<List<PredicateOperator>>() {
-                Models.GetPredicateOperatorList(new List<string>() { "(have_image Star1 infrared1)", "(have_image star1 thermograph1)" }),
-                Models.GetPredicateOperatorList(new List<string>() { "(have_image star2 infrared2)", "(have_image star2 thermograph2)" }),
-                Models.GetPredicateOperatorList(new List<string>() { "(have_image star3 infrared3)" }),
-            };
+
             List<Problem> problems = Models.GetProblemList(names, initialstates, goalstates);
-            List<string> expected = new List<string>()
-            {
-                "init_on(b1,b1,p1).", "init_calibration_target(instrument1,star1,p1).", "init_power_avail(satellite1,p1).",
-                "init_on(b2,b2,p2).", "init_calibration_target(instrument2,star2,p2).", "init_power_avail(satellite2,p2).",
-                "init_on(b3,b3,p3).", "init_calibration_target(instrument3,star3,p3).", "init_power_avail(satellite3,p3).",
-                "goal_have_image(star1,infrared1,p1).", "goal_have_image(star1,thermograph1,p1).",
-                "goal_have_image(star2,infrared2,p2).", "goal_have_image(star2,thermograph2,p2).",
-                "goal_have_image(star3,infrared3,p3)."
-            };
 
             List<string> result = backgroundGenerator.GetPredicates(problems);
 
             CollectionAssert.AreEquivalent(expected, result);
+        }
+
+        public static IEnumerable<object[]> PredicateInput
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] {
+                        new List<string>() { "p1", "p2", "p3" },
+                        new List<List<PredicateOperator>>() {
+                            Models.GetPredicateOperatorList(new List<string>() { "(on b1 b1)", "(calibration_target instrument1 star1)", "(power_avail satellite1)" }),
+                            Models.GetPredicateOperatorList(new List<string>() { "(on b2 b2)", "(calibration_target instrument2 star2)", "(power_avail satellite2)" }),
+                            Models.GetPredicateOperatorList(new List<string>() { "(on b3 b3)", "(calibration_target Instrument3 star3)", "(power_avail satellite3)" }),
+                        },
+                        new List<List<PredicateOperator>>() {
+                            Models.GetPredicateOperatorList(new List<string>() { "(have_image Star1 infrared1)", "(have_image star1 thermograph1)" }),
+                            Models.GetPredicateOperatorList(new List<string>() { "(have_image star2 infrared2)", "(have_image star2 thermograph2)" }),
+                            Models.GetPredicateOperatorList(new List<string>() { "(have_image star3 infrared3)" }),
+                        },
+                        new List<string>()
+                        {
+                            "init_on(b1,b1,p1).", "init_calibration_target(instrument1,star1,p1).", "init_power_avail(satellite1,p1).",
+                            "init_on(b2,b2,p2).", "init_calibration_target(instrument2,star2,p2).", "init_power_avail(satellite2,p2).",
+                            "init_on(b3,b3,p3).", "init_calibration_target(instrument3,star3,p3).", "init_power_avail(satellite3,p3).",
+                            "goal_have_image(star1,infrared1,p1).", "goal_have_image(star1,thermograph1,p1).",
+                            "goal_have_image(star2,infrared2,p2).", "goal_have_image(star2,thermograph2,p2).",
+                            "goal_have_image(star3,infrared3,p3)."
+                        }
+                    }
+                };
+            }
         }
     }
 }
--- PDAI/Test/Unit/PopperWriter.Tests/ExampleGeneratorTests.cs
+++ PDAI/Test/Unit/PopperWriter.Tests/ExampleGeneratorTests.cs
@@ -24,36 +24,49 @@ namespace PopperWriter.Tests
             Assert.AreEqual(expected, result);
         }
 
-        
-        //[DataTestMethod]
-        //public void GetActions_validAction_CorrectString()
-        //{
-        //    ExampleGenerator exampleGenerator = new ExampleGenerator();
-        //    List<string> names = new List<string>() { "p1", "p2", "p3" };
-        //    List<List<ActionOperator>> goodoperators = new List<List<ActionOperator>>() {
-        //        Models.GetActionOperatorList(new List<string>() { "drive truck0 depot0 distributor0", "drop hoist0 crate0 pallet0 depot0", "lift hoist0 crate2 pallet0 depot0"}),
-        //        Models.GetActionOperatorList(new List<string>() { "drive truck1 distributor0 depot0", "lift hoist1 crate0 pallet1 distributor0", "unload hoist0 crate0 truck0 depot0" }),
-        //        Models.GetActionOperatorList(new List<string>() { "drive truck0 depot0 distributor0", "drop hoist1 crate0 pallet1 distributor0", "load hoist0 crate0 truck0 depot0" })
-        //    };
-        //    List<List<ActionOperator>> badoperators = new List<List<ActionOperator>>() {
-        //        Models.GetActionOperatorList(new List<string>() { "drive truck0 distributor0 distributor0", "drive truck1 distributor0 distributor0", "drive truck0 depot0 depot0"}),
-        //        Models.GetActionOperatorList(new List<string>() { "drive truck0 depot0 depot0", "drive truck1 depot0 depot0", "drive truck0 distributor0 distributor0" }),
-        //        Models.GetActionOperatorList(new List<string>() { "drive truck0 depot0 depot0", "drive truck1 distributor0 depot0", "drive truck1 distributor0 distributor0"})
-        //    };
-        //    List<Problem> problems = Models.GetProblemList(names, goodoperators, badoperators);
-        //    List<string> expected = new List<string>()
-        //    {
-        //        "pos(drive(truck0,depot0,distributor0,p1)).", "pos(drop(hoist0,crate0,pallet0,depot0,p1)).", "pos(lift(hoist0,crate2,pallet0,depot0,p1)).",
-        //        "pos(drive(truck1,distributor0,depot0,p2)).", "pos(lift(hoist1,crate0,pallet1,distributor0,p2)).", "pos(unload(hoist0,crate0,truck0,depot0,p2)).",
-        //        "pos(drive(truck0,depot0,distributor0,p3)).", "pos(drop(hoist1,crate0,pallet1,distributor0,p3)).", "pos(load(hoist0,crate0,truck0,depot0,p3)).",
-        //        "neg(drive(truck0,distributor0,distributor0,p1)).", "neg(drive(truck1,distributor0,distributor0,p1)).", "neg(drive(truck0,depot0,depot0,p1)).",
-        //        "neg(drive(truck0,depot0,depot0,p2)).", "neg(drive(truck1,depot0,depot0,p2)).", "neg(drive(truck0,distributor0,distributor0,p2)).",
-        //        "neg(drive(truck0,depot0,depot0,p3)).", "neg(drive(truck1,distributor0,depot0,p3)).", "neg(drive(truck1,distributor0,distributor0,p3))."
-        //    };
+        /*
+        [DynamicData("ActionInput")]
+        [DataTestMethod]
+        public void GetActions_validAction_CorrectString(List<string> names, List<List<ActionOperator>> goodoperators, List<List<ActionOperator>> badoperators, List<string> expected)
+        {
+            ExampleGenerator exampleGenerator = new ExampleGenerator();
+            List<Problem> problems = Models.GetProblemList(names, goodoperators, badoperators);
 
-        //    List<string> result = exampleGenerator.GetActions(problems);
+            List<string> result = exampleGenerator.GetActions(null, problems);
 
-        //    CollectionAssert.AreEquivalent(expected, result);
-        //}
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+
+        public static IEnumerable<object[]> ActionInput
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] {
+                        new List<string>() { "p1", "p2", "p3" },
+                        new List<List<ActionOperator>>() {
+                            Models.GetActionOperatorList(new List<string>() { "drive truck0 depot0 distributor0", "drop hoist0 crate0 pallet0 depot0", "lift hoist0 crate2 pallet0 depot0" }),
+                            Models.GetActionOperatorList(new List<string>() { "drive truck1 distributor0 depot0", "lift hoist1 crate0 pallet1 distributor0", "unload hoist0 crate0 truck0 depot0" }),
+                            Models.GetActionOperatorList(new List<string>() { "drive truck0 depot0 distributor0", "drop hoist1 crate0 pallet1 distributor0", "load hoist0 crate0 truck0 depot0" })
+                        },
+                        new List<List<ActionOperator>>() {
+                            Models.GetActionOperatorList(new List<string>() { "drive truck0 distributor0 distributor0", "drive truck1 distributor0 distributor0", "drive truck0 depot0 depot0"}),
+                            Models.GetActionOperatorList(new List<string>() { "drive truck0 depot0 depot0", "drive truck1 depot0 depot0", "drive truck0 distributor0 distributor0" }),
+                            Models.GetActionOperatorList(new List<string>() { "drive truck0 depot0 depot0", "drive truck1 distributor0 depot0", "drive truck1 distributor0 distributor0"})
+                        },
+                        new List<string>()
+                        {
+                            "pos(drive(truck0,depot0,distributor0,p1)).", "pos(drop(hoist0,crate0,pallet0,depot0,p1)).", "pos(lift(hoist0,crate2,pallet0,depot0,p1)).",
+                            "pos(drive(truck1,distributor0,depot0,p2)).", "pos(lift(hoist1,crate0,pallet1,distributor0,p2)).", "pos(unload(hoist0,crate0,truck0,depot0,p2)).",
+                            "pos(drive(truck0,depot0,distributor0,p3)).", "pos(drop(hoist1,crate0,pallet1,distributor0,p3)).", "pos(load(hoist0,crate0,truck0,depot0,p3)).",
+                            "neg(drive(truck0,distributor0,distributor0,p1)).", "neg(drive(truck1,distributor0,distributor0,p1)).", "neg(drive(truck0,depot0,depot0,p1)).",
+                            "neg(drive(truck0,depot0,depot0,p2)).", "neg(drive(truck1,depot0,depot0,p2)).", "neg(drive(truck0,distributor0,distributor0,p2)).",
+                            "neg(drive(truck0,depot0,depot0,p3)).", "neg(drive(truck1,distributor0,depot0,p3)).", "neg(drive(truck1,distributor0,distributor0,p3))."
+                        }
+                    }
+                };
+            }
+        }*/
     }
 }
