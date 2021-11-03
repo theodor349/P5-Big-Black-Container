9ab42ee6b499ae910d73ae419d1eb1f8a0ad25a0
esb179 <esbenn179@gmail.com>
2021-11-03 11:18:23 +0100

    Direction

--- PDAI/PDAI/DataGenerator.cs
+++ PDAI/PDAI/DataGenerator.cs
@@ -22,8 +22,8 @@ namespace PDAI
             string domainFolderPath = Path.Combine(rootBbcFolder, "domainfiles", Path.GetFileName(inputFolderPath));
             var actionsPaths = Directory.GetDirectories(domainFolderPath).ToList();
 
-            var biasEnumerator = new BiasAllEnumerator();
-            int iterations = 5;
+            var biasEnumerator = new BiasRandomEnumerator();
+            int iterations = 6;
             int beta = 2;
             int maxRuntime = 1 * 4 * 60 * 1000; // hour, min, sec, ms 
 
