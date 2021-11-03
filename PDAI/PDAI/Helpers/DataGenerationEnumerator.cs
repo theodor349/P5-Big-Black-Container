7a13c2f7f2c7290250c49ffb385ffe3abe7b0533
Theodor Risager <theodor349@gmail.com>
2021-11-03 08:45:59 +0100

    Enumerators

--- PDAI/PDAI/Helpers/DataGenerationEnumerator.cs
+++ PDAI/PDAI/Helpers/DataGenerationEnumerator.cs
@@ -24,7 +24,7 @@ namespace PDAI.Helpers
         {
             return new BiasIncrement()
             {
-                Var = iteration % int.MaxValue,
+                Var = 1,
                 Body = 0,
                 Clause = 0,
             };
@@ -43,4 +43,59 @@ namespace PDAI.Helpers
             };
         }
     }
+
+    public class BiasRandomEnumerator : IBiasEnumerator
+    {
+        public BiasIncrement GetIncrement(int iteration)
+        {
+            var rand = new Random().Next();
+
+            return new BiasIncrement()
+            {
+                Var = rand % 3 == 0 ? 1 : 0,
+                Body = rand % 3 == 1 ? 1 : 0,
+                Clause = rand % 3 == 2 ? 1 : 0,
+            };
+        }
+    }
+
+    public class BiasBodyEnumerator : IBiasEnumerator
+    {
+        public BiasIncrement GetIncrement(int iteration)
+        {
+            return new BiasIncrement()
+            {
+                Var = 0,
+                Body = 1,
+                Clause = 0,
+            };
+        }
+    }
+
+    public class BiasClauseEnumerator : IBiasEnumerator
+    {
+        public BiasIncrement GetIncrement(int iteration)
+        {
+            return new BiasIncrement()
+            {
+                Var = 0,
+                Body = 0,
+                Clause = 1,
+            };
+        }
+    }
+
+    public class BiasAllReverseEnumerator : IBiasEnumerator
+    {
+        public BiasIncrement GetIncrement(int iteration)
+        {
+            return new BiasIncrement()
+            {
+                Var = iteration % 3 >= 2 ? 1 : 0,
+                Body = iteration % 3 >= 1 ? 1 : 0,
+                Clause = iteration % 3 >= 0 ? 1 : 0,
+            };
+        }
+    }
+
 }
