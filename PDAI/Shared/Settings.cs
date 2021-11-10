8f8334b125c6f98f7989fe5d1a867ec8a280190c
Theodor Risager <theodor349@gmail.com>
2021-11-10 11:14:58 +0100

    Settings file

---
+++ PDAI/Shared/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Settings
+    {
+        public static Settings Current;
+
+        public string DomainFolder { get; set; }    // d    --domain-folder
+        public string TargetFolder { get; set; }    // t
+        public int MaxProblems { get; set; }        // p
+        public double SplitPercent { get; set; }    // s
+        public int NumChunks { get; set; }          // c
+        public bool RunInfinite { get; set; }       // r
+        public string ActionToRun { get; set; }     // a
+        public int MaxRuntime { get; set; }         // R
+        public int Beta { get; set; }               // b
+        public int Iterations { get; set; }         // i
+
+        public Settings(string[] args)
+        {
+            
+        }
+    }
+}
