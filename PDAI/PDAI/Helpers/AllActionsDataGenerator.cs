cc833d3fb02613ebf6dbd3af482a3309ebc8f834
Alan Khorsid <alan18@hotmail.dk>
2021-11-19 12:03:34 +0100

    Create AllActionsDataGenerator.cs

---
+++ PDAI/PDAI/Helpers/AllActionsDataGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Shared;
+
+namespace PDAI.Helpers
+{
+    public class AllActionsDataGenerator
+    {
+        private readonly Settings _settings;
+
+        public AllActionsDataGenerator()
+        {
+        }
+
+    }
+}
